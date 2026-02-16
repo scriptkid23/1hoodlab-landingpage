a52b1f1fbc9648cb8f9b3fbd28bf48dbfbbf1d7d
hoando <hoando.dev@gmail.com>
2026-02-16 22:49:56 +0700

    remove

--- app/template.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { PageTransitionProvider } from "@/components/PageTransition";
-
-export default function Template({ children }: { children: React.ReactNode }) {
-  return <PageTransitionProvider>{children}</PageTransitionProvider>;
-}
-
